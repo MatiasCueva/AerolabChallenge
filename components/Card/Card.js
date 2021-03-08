7c429968bc8906d5f47e18da6d31ba83121fa2f1
Matias Cueva <matias.acueva@hotmail.com>
2021-03-08 18:23:39 -0300

    init

---
+++ components/Card/Card.js
@@ -0,0 +1,11 @@
+import styles from "./Card.module.scss";
+
+export default function Card() {
+  return (
+    <>
+      <div className={styles.card}>
+        <p>tarjeta productos</p>
+      </div>
+    </>
+  );
+}
---
+++ components/Header/Header.js
@@ -0,0 +1,15 @@
+import styles from "./Header.module.scss";
+
+export default function Header() {
+  return (
+    <>
+      <div className={styles.headerBg}>
+        <div className={styles.container}>
+          <div className={styles.header}>
+            <h2>Electronics</h2>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
